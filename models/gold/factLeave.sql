7bd82793a8efdefe749ceebfc1187c92d6042e09
Mani G <manigillgm1980@gmail.com>
2025-09-06 09:06:31 +0000

    Derived leave dates

--- models/gold/factLeave.sql
+++ models/gold/factLeave.sql
@@ -5,10 +5,10 @@
 ) }}
 
 
-SELECT b.employeeid, c.department_sk, d.leavetype_sk, r.region_sk
-,s.shift_sk, a.vacationhours, a.sickleavehours
+SELECT a.date_sk, b.employeeid as employee_sk, c.department_sk, d.leavetype_sk, r.region_sk
+,s.shift_sk, a.hours
 FROM {{ref("transform_factLeave")}} a
-LEFT JOIN {{ref("dimEmployee")}} b ON a.employeeid = b.employeeid AND b.dbt_valid_to IS NULL
+LEFT JOIN {{ref("dimEmployee")}} b ON a.employee_sk = b.employeeid AND b.dbt_valid_to IS NULL
 LEFT JOIN {{ref("dimDepartment")}} c ON a.departmentid = c.departmentid
 LEFT JOIN {{ref("dimLeaveType")}} d ON a.leavetype = d.leavetype
 LEFT JOIN {{ref("dimRegion")}} r ON a.regionid = r.regionid
--- models/silver/transform_factLeave.sql
+++ models/silver/transform_factLeave.sql
@@ -4,14 +4,15 @@
 ) }}
 
 select 
-    a.BUSINESSENTITYID EMPLOYEEID, a.EmployeeName
+    a.DATE AS date_sk,
+    a.BUSINESSENTITYID employee_sk, a.EmployeeName
     , c.departmentid, c.departmentname
     , l.leavetype, l.leavetype_sk
     , IFNULL(r.regionid,99) regionid, IFNULL(r.region, 'Unknown') region
     , s.shiftid, s.shiftname
     , a.Hours
 from {{ ref("vw_factLeave") }} a
-LEFT JOIN {{ ref("vw_employeedepartmenthistory") }} b ON a.EMPLOYEEID = b.businessentityid
+LEFT JOIN {{ ref("vw_employeedepartmenthistory") }} b ON a.BUSINESSENTITYID = b.businessentityid
 LEFT JOIN {{ ref("transform_dimDepartment")}} c ON b.departmentid = c.departmentid
 LEFT JOIN {{ ref("transform_dimShift")}} s ON b.shiftid = s.shiftid
 LEFT JOIN {{ ref("transform_dimRegion")}} r ON c.departmentid = r.REGIONID
