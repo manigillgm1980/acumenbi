a6f74b5dc3a69f968bfeaa017a79cfc28109aee2
Mani G <manigillgm1980@gmail.com>
2025-09-06 08:42:29 +0000

    x

--- models/silver/transform_factLeave.sql
+++ models/silver/transform_factLeave.sql
@@ -4,39 +4,18 @@
 ) }}
 
 select 
-    a.EMPLOYEEID, REPLACE(a.LOGINID, 'adventure-works\\','') AS EmployeeName
+    a.BUSINESSENTITYID EMPLOYEEID, a.EmployeeName
     , c.departmentid, c.departmentname
     , l.leavetype, l.leavetype_sk
     , IFNULL(r.regionid,99) regionid, IFNULL(r.region, 'Unknown') region
     , s.shiftid, s.shiftname
-    , a.vacationhours
-    , 0 sickleavehours
-from {{ ref("transform_dimEmployee") }} a
+    , a.Hours
+from {{ ref("vw_factLeave") }} a
 LEFT JOIN {{ ref("vw_employeedepartmenthistory") }} b ON a.EMPLOYEEID = b.businessentityid
 LEFT JOIN {{ ref("transform_dimDepartment")}} c ON b.departmentid = c.departmentid
 LEFT JOIN {{ ref("transform_dimShift")}} s ON b.shiftid = s.shiftid
 LEFT JOIN {{ ref("transform_dimRegion")}} r ON c.departmentid = r.REGIONID
-LEFT JOIN {{ ref("transform_dimLeaveType")}} l ON l.leavetype = 'Vacation'
-WHERE a.VACATIONHOURS >0
-
-
-UNION ALL
-
-select 
-    a.EMPLOYEEID, REPLACE(a.LOGINID, 'adventure-works\\','') AS EmployeeName
-    , c.departmentid, c.departmentname
-    , l.leavetype, l.leavetype_sk
-    , IFNULL(r.regionid,99) regionid, IFNULL(r.region, 'Unknown') region
-    , s.shiftid, s.shiftname
-    , 0 vacationhours
-    , a.sickleavehours
-from {{ ref("transform_dimEmployee") }} a
-LEFT JOIN {{ ref("vw_employeedepartmenthistory") }} b ON a.EMPLOYEEID = b.businessentityid
-LEFT JOIN {{ ref("transform_dimDepartment")}} c ON b.departmentid = c.departmentid
-LEFT JOIN {{ ref("transform_dimShift")}} s ON b.shiftid = s.shiftid
-LEFT JOIN {{ ref("transform_dimRegion")}} r ON c.departmentid = r.REGIONID
-LEFT JOIN {{ ref("transform_dimLeaveType")}} l ON l.leavetype = 'Sick'
-WHERE a.SICKLEAVEHOURS >0
+LEFT JOIN {{ ref("transform_dimLeaveType")}} l ON l.leavetype = a.LeaveType
 
 
 
